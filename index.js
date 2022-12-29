11a7efe4f7bb102d6bf36dc89c64b639ac57acbf
arun-ar7 <arunsmart1202@gmail.com>
2022-12-29 15:55:38 +0530

    created actions and reducers

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("index.js is running");
+const redux = require("redux")
+const createStore = redux.createStore
+
+const buy_cake = "BUY_CAKE"
+function buyCake(){
+    return {
+        type : buy_cake,
+        info : "first redux action"
+    }
+}
+
+//initial state value
+const initialState = {
+    numOfCakes : 10
+}
+
+//reducer function with two arguments
+const reducer = (state = initialState, action)=>{
+    switch (action.type){
+        case BUY_CAKE : 
+            return {
+                ...state,
+                numOfCakes : state.numOfCakes-1
+            };
+        default : return state
+    }
+
+}
